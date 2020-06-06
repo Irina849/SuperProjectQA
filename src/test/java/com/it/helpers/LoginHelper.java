7c1ccb3ca46f810b78c39432bf75945618aa6e2a
Brambuca <yridze849@gmail.com>
2020-06-06 08:58:52 +0300

    add log4j log (loginhelper) to  allure

--- src/test/java/com/it/helpers/LoginHelper.java
+++ src/test/java/com/it/helpers/LoginHelper.java
@@ -2,6 +2,7 @@ package com.it.helpers;
 
 import com.it.pages.LoginPage;
 import com.it.users.User;
+import io.qameta.allure.Step;
 
 public class LoginHelper extends LoginPage {
 
@@ -10,9 +11,10 @@ public class LoginHelper extends LoginPage {
         setInputPassword(password);
         btnClickLogin();
     }
-
+    @Step("login user name - {user.userName}, password - {user.password}")
     public void login(User user) {
         log.info(String.format("login user name - %s , password - %s .", user.userName, user.password));
+
         login(user.userName, user.password);
     }
 }
